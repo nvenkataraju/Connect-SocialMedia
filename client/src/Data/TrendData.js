dc091e766702dcb1cf5d71eed13b4b81487f8cac
neshavenkataraju <venkatarajunesha260@gmail.com>
2022-11-11 16:51:53 +0530

    social

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "Data Science",
+    shares: 97,
+  },
+  {
+    name: "Avangers",
+    shares: 80.5,
+  },
+  {
+    name: "Blockchain",
+    shares: 75.5,
+  },
+  {
+    name: "SpaceX",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "Crypto",
+    shares: 20,
+  },
+];
